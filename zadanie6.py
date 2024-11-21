7de891c16130fcb406069516d426c2deec4945d2
Apoloniia2007 <pola.prokurat@gmail.com>
2024-11-21 11:15:36 +0100

    Update zadanie6.py

---
+++ zadanie6.py
@@ -0,0 +1,1 @@
+różnica jest taka ze w przypadku petli for warunek jest sprawdzany na poczatku petli czyli petla moze sie nie wykonac ani razu a w przypadku petli do while ktora jest wykonana co najmniej raz
