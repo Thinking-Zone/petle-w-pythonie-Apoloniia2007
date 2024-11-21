54e806538b90e806727b55925fc4aee75a076eeb
Apoloniia2007 <pola.prokurat@gmail.com>
2024-11-21 11:29:29 +0100

    Update zadanie10.py

---
+++ zadanie10.py
@@ -0,0 +1,6 @@
+# Pętla do iteracji po liczbach od 1 do 10
+for liczba in range(1, 11):
+    if liczba % 2 == 0:
+        print(f"{liczba} - Parzysta")
+    else:
+        print(f"{liczba} - Nieparzysta")
